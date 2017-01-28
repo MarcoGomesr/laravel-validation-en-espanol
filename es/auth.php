0d2248a4b1f27966c83dc95e75a2eaabbc66c981
zero-void <nachopolese@outlook.com>
2017-01-28 02:02:33 -0300

    Agregado al igual que en 5.4

---
+++ es/auth.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'failed' => 'Las credenciales introducidas son incorrectas.',
+    'throttle' => 'Demasiados intentos de acceso. Inténtelo de nuevo en :seconds segundos.',
+
+];
