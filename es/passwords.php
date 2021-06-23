827a635f75e608704b2661a6101de82a2bba61d1
Lito <lito@eordes.com>
2021-06-23 22:23:28 +0200

    Updated passwords.php code style

--- es/passwords.php
+++ es/passwords.php
@@ -2,16 +2,16 @@
 
 return [
 
-	/*
-	|--------------------------------------------------------------------------
-	| Restablecer contraseña de líneas de idioma
-	|--------------------------------------------------------------------------
-	|
-        | Las siguientes líneas de idioma son las líneas predeterminadas que coinciden con las razones
-        | que proporciona la contraseñas de los usuario para un intento de actualización que
-	| es invalida, por ejemplo, para un token no válido o una nueva contraseña no válida.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Restablecer contraseña de líneas de idioma
+    |--------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de idioma son las líneas predeterminadas que coinciden con las razones
+    | que proporciona la contraseñas de los usuario para un intento de actualización que
+    | es invalida, por ejemplo, para un token no válido o una nueva contraseña no válida.
+    |
+    */
 
     'password' => 'Las contraseñas deben contener al menos 6 caracters y coincidir.',
     'reset' => 'Su contraseña ha sido cambiada.',
